30497877b3dc3e8b03c48616c08e508af42e77e0
Vicente Ángel Gutiérrez <45756687+vicentecom@users.noreply.github.com>
2021-05-10 15:36:30 +0200

    [FIX] pms_pwa: Multiple and single modal fixes. (#33)
    * [FIX] pms_pwa: Multiple and single modal fixes.
    
    * [FIX] caught error when reservation is canceled in pending_amount
    
    Co-authored-by: Manuel Alejandro Núñez Liz <lordchucu@gmail.com>

--- pms_pwa/static/src/js/o_pms_pwa_reservation_table_filter.js
+++ pms_pwa/static/src/js/o_pms_pwa_reservation_table_filter.js
@@ -32,10 +32,16 @@ odoo.define("pms_pwa.reservation_table", function (require) {
                 date_options
             );
             $("#o_pms_pwa_new_reservation_modal")
-                .find("input[name='range_check_date_modal']")
+                .find("input[name='range_check_date_modal_reservation']")
                 .val(checkin_date + " - " + checkout_date);
             $("#o_pms_pwa_new_reservation_modal")
-                .find("input[name='range_check_date_modal']")
+                .find("input[name='range_check_date_modal_reservation']")
+                .trigger("change");
+            $("#o_pms_pwa_new_reservation_modal")
+                .find("input[name='range_check_date_modal_reservation_multi']")
+                .val(checkin_date + " - " + checkout_date);
+            $("#o_pms_pwa_new_reservation_modal")
+                .find("input[name='range_check_date_modal_reservation_multi']")
                 .trigger("change");
         }, 500);
     });
@@ -47,8 +53,10 @@ odoo.define("pms_pwa.reservation_table", function (require) {
             "allowed_board_service_room_ids",
             "allowed_channel_type_ids",
             "allowed_pricelists",
+            "room_types",
+            "room_numbers",
         ];
-        if (event.currentTarget.name == "range_check_date_modal") {
+        if (event.currentTarget.name == "range_check_date_modal_reservation") {
             let value_range_picker = event.currentTarget.value;
             values.checkin = value_range_picker.split(" - ")[0];
             values.checkout = value_range_picker.split(" - ")[1];
@@ -112,7 +120,7 @@ odoo.define("pms_pwa.reservation_table", function (require) {
     $("form#single_reservation_form").on("submit", function (event) {
         event.preventDefault();
         var values = $("form#single_reservation_form").serializeArray();
-        if (event.currentTarget.name == "range_check_date_modal") {
+        if (event.currentTarget.name == "range_check_date_modal_reservation") {
             let value_range_picker = event.currentTarget.value;
 
             values.checkin = value_range_picker.split(" - ")[0];
@@ -152,6 +160,8 @@ odoo.define("pms_pwa.reservation_table", function (require) {
             "allowed_board_service_room_ids",
             "allowed_channel_type_ids",
             "allowed_pricelists",
+            "room_types",
+            "room_numbers",
         ];
         try {
             var id = $("#multiple_reservation_form input[name='id']").val();
@@ -159,7 +169,7 @@ odoo.define("pms_pwa.reservation_table", function (require) {
         } catch (error) {
             console.log(error);
         }
-        if (event.currentTarget.name == "range_check_date_modal") {
+        if (event.currentTarget.name == "range_check_date_modal_reservation_multi") {
             let value_range_picker = event.currentTarget.value;
             values.checkin = value_range_picker.split(" - ")[0];
             values.checkout = value_range_picker.split(" - ")[1];
@@ -779,12 +789,16 @@ odoo.define("pms_pwa.reservation_table", function (require) {
                                                             ).reservation.price_total;
 
                                                             // refresh pending amount
-                                                            let b = document.getElementsByClassName(
-                                                                "pending_amount"
-                                                            );
-                                                            b[0].innerText = JSON.parse(
-                                                                new_data
-                                                            ).reservation.folio_pending_amount;
+                                                            try {
+                                                                let b = document.getElementsByClassName(
+                                                                    "pending_amount"
+                                                                );
+                                                                b[0].innerText = JSON.parse(
+                                                                    new_data
+                                                                ).reservation.folio_pending_amount;
+                                                            } catch (error) {
+                                                                console.log(error);
+                                                            }
                                                         }
                                                     });
                                                 });
@@ -937,13 +951,17 @@ odoo.define("pms_pwa.reservation_table", function (require) {
                                                                 new_data
                                                             ).reservation.price_total;
 
-                                                            // refresh pending amount
-                                                            let b = document.getElementsByClassName(
-                                                                "pending_amount"
-                                                            );
-                                                            b[0].innerText = JSON.parse(
-                                                                new_data
-                                                            ).reservation.folio_pending_amount;
+                                                            // refresh pending amount.
+                                                            try {
+                                                                let b = document.getElementsByClassName(
+                                                                    "pending_amount"
+                                                                );
+                                                                b[0].innerText = JSON.parse(
+                                                                    new_data
+                                                                ).reservation.folio_pending_amount;
+                                                            } catch (error) {
+                                                                console.log(error);
+                                                            }
                                                         }
                                                     });
                                                 });
